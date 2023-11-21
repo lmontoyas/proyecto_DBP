71a14edb917d096264713a98d36cb215879703c7
lmontoyas <83974237+lmontoyas@users.noreply.github.com>
2023-11-21 03:13:57 -0500

    Delete back-end directory

--- back-end/src/main/java/com/example/demo/calendario/repository/CalendarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.calendario.repository;
-
-import com.example.demo.calendario.model.Calendario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CalendarioRepository extends JpaRepository<Calendario,Long> {
-}
--- back-end/src/main/java/com/example/demo/exception/model/DataIntegrityViolationException.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.exception.model;
-
-public class DataIntegrityViolationException extends RuntimeException{
-
-
-    public DataIntegrityViolationException(String message) {
-        super(message);
-    }
-}
